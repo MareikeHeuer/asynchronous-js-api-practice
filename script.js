6390a8b471ac50ca94c8534e6cb00fb1725f2c32
Mareike Heuer <mareike.heuer@hotmail.com>
2021-07-28 10:48:47 +0200

    Example of event loop in practice

--- script.js
+++ script.js
@@ -163,4 +163,15 @@ btn.addEventListener('click', function () {
   getCountryData('germany');
 });
 
-getCountryData('Australia');
+getCountryData('Germany');
+
+//// Event Loop in Practice
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved Promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+console.log('Test end');
